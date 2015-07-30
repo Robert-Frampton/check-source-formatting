5c4da5edc6565b2823e5ca40b9c48825af3b3d07
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-07-30 12:16:34 -0700

    Allow arguments to be extendable

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
 var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
 var updateNotifier = require('update-notifier');
-
-
-var MAP_OMIT = {
-	'$0': true,
-	'_': true
-};
 
 var notifier = updateNotifier(
 	{
@@ -19,25 +15,62 @@ if (notifier.update) {
 	notifier.notify();
 }
 
-var argv = require('../lib/argv');
-
-var flags = _.reduce(
-	argv,
-	function(res, item, index) {
-		if (index.length > 1 && !MAP_OMIT[index]) {
-			index = _.camelCase(index);
-
-			res[index] = item;
-		}
-
-		return res;
-	},
-	{}
-);
-
 var CLI = require('cli-base').CLI;
 
-new CLI({
-	args: argv._,
-	flags: flags
-}).init();
+var cliInstance = new CLI({
+	formattersPath: path.join(__dirname, '../formatters'),
+	options: {
+		'display-raw': {
+			boolean: true,
+			default: false
+		},
+		l: {
+			alias: 'lint',
+			boolean: true,
+			default: true
+		},
+		m: {
+			alias: 'check-metadata',
+			boolean: true,
+			default: false
+		},
+		v: {
+			alias: 'verbose',
+			boolean: true,
+			default: false
+		}
+	}
+});
+
+var hasModulesFile = function(fileDir) {
+	return fs.existsSync(path.join(fileDir, 'modules.js'));
+};
+
+cliInstance.plug({
+	afterProcess: function(done) {
+		if (this._CHECK_META) {
+			require('../lib/meta').check(
+				{
+					done: done,
+					liferayModuleDir: this._liferayModuleDir,
+					verbose: this.flags.verbose
+				}
+			);
+		}
+		else {
+			done();
+		}
+	},
+	processFile: function(file) {
+		if (!this._CHECK_META && this.flags.checkMetadata && path.extname(file) === '.js') {
+			var fileDir = path.dirname(path.resolve(file));
+
+			if (path.basename(fileDir) === 'liferay' && hasModulesFile(fileDir)) {
+				this._CHECK_META = true;
+				this._liferayModuleDir = fileDir;
+			}
+		}
+	}
+});
+
+cliInstance.init();
